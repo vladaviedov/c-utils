8d29596827f3dc1be106a14cbd48137f3db4685b
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-04-07 16:20:30 -0400

    [unicode] add utf8 parse tests

--- unicode/test/uchar_tests.cpp
+++ unicode/test/uchar_tests.cpp
@@ -6,21 +6,63 @@ extern "C" {
 #include <cstring>
 #include <gtest/gtest.h>
 
-static const uchar uc_null_str[] = { 0 };
+static const uchar uc_empty[] = { 0 };
+static const uint64_t uc_len_empty = 0;
+static const char *utf8_empty = "";
+
 static const uchar uc_big_points[]
 	= { 'a', 'b', 'c', 0xff000000, 'd', 'e', 0x0011ffff, 0 };
 
 static const uchar uc_ascii[]
 	= { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', 0 };
+static const uint64_t uc_len_ascii = 11;
 static const char *utf8_ascii = "hello world";
 
 static const uchar uc_multibyte1[]
 	= { U'п', U'р', U'и', U'в', U'е', U'т', ' ', U'м', U'и', U'р', 0 };
+static const uint64_t uc_len_multibyte1 = 10;
 static const char *utf8_multibyte1 = "привет мир";
 
 static const uchar uc_multibyte2[]
 	= { U'こ', U'ん', U'に', U'ち', U'は', U'世', U'界', 0 };
+static const uint64_t uc_len_multibyte2 = 7;
 static const char *utf8_multibyte2 = "こんにちは世界";
+
+TEST(Unicode, ParseNull) {
+	uchar *result = utf8_parse(nullptr);
+
+	EXPECT_EQ(result, nullptr);
+}
+
+TEST(Unicode, ParseEmpty) {
+	uchar *result = utf8_parse(utf8_empty);
+
+	ASSERT_NE(result, nullptr);
+	EXPECT_EQ(memcmp(result, uc_empty, uc_len_empty + 1), 0);
+
+	free(result);
+}
+
+TEST(Unicode, ParseAscii) {
+	uchar *result = utf8_parse(utf8_ascii);
+
+	ASSERT_NE(result, nullptr);
+	EXPECT_EQ(memcmp(result, uc_ascii, uc_len_ascii + 1), 0);
+
+	free(result);
+}
+
+TEST(Unicode, ParseMultibyte) {
+	uchar *result1 = utf8_parse(utf8_multibyte1);
+	ASSERT_NE(result1, nullptr);
+	EXPECT_EQ(memcmp(result1, uc_multibyte1, uc_len_multibyte1 + 1), 0);
+	free(result1);
+
+	uchar *result2 = utf8_parse(utf8_multibyte2);
+	ASSERT_NE(result2, nullptr);
+	EXPECT_EQ(memcmp(result2, uc_multibyte2, uc_len_multibyte2 + 1), 0);
+	free(result2);
+}
 
 TEST(Unicode, EncodeImpossible) {
 	char *result = utf8_encode(uc_big_points);
@@ -35,11 +77,10 @@ TEST(Unicode, EncodeNull) {
 }
 
 TEST(Unicode, EncodeEmpty) {
-	char *result = utf8_encode(uc_null_str);
+	char *result = utf8_encode(uc_empty);
 
-	EXPECT_NE(result, nullptr);
-	EXPECT_EQ(*result, '\0');
-	EXPECT_EQ(strlen(result), 0);
+	ASSERT_NE(result, nullptr);
+	EXPECT_STREQ(result, utf8_empty);
 
 	free(result);
 }
@@ -47,7 +88,7 @@ TEST(Unicode, EncodeEmpty) {
 TEST(Unicode, EncodeAscii) {
 	char *result = utf8_encode(uc_ascii);
 
-	EXPECT_NE(result, nullptr);
+	ASSERT_NE(result, nullptr);
 	EXPECT_STREQ(result, utf8_ascii);
 
 	free(result);
@@ -55,13 +96,12 @@ TEST(Unicode, EncodeAscii) {
 
 TEST(Unicode, EncodeMultibyte) {
 	char *result1 = utf8_encode(uc_multibyte1);
-	char *result2 = utf8_encode(uc_multibyte2);
-
-	EXPECT_NE(result1, nullptr);
-	EXPECT_NE(result2, nullptr);
+	ASSERT_NE(result1, nullptr);
 	EXPECT_STREQ(result1, utf8_multibyte1);
-	EXPECT_STREQ(result2, utf8_multibyte2);
-
 	free(result1);
+
+	char *result2 = utf8_encode(uc_multibyte2);
+	ASSERT_NE(result2, nullptr);
+	EXPECT_STREQ(result2, utf8_multibyte2);
 	free(result2);
 }
