1e988816915a10d06b7c0ddebcc972cc4d88812d
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-04-27 11:16:56 -0400

    [unicode] implement ustrcpy, ustrdup, ustrlen

---
+++ unicode/src/ustring.c
@@ -0,0 +1,86 @@
+/**
+ * @file ustring.c
+ * @author Vladyslav Aviedov <vladaviedov at protonmail dot com>
+ * @version dev
+ * @date 2025
+ * @license LGPLv3.0
+ * @brief string.h equivalent for unicode point strings.
+ */
+#include "ustring.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "uchar.h"
+
+uchar *ustrcpy(uchar *restrict dst, const uchar *restrict src) {
+	uchar *restrict trav = dst;
+
+	do {
+		*trav++ = *src;
+	} while (*src++ != 0);
+
+	return dst;
+}
+
+uchar *ustrncpy(uchar *restrict dst, const uchar *restrict src, size_t dsize) {
+	if (dsize == 0) {
+		return dst;
+	}
+
+	uchar *restrict trav = dst;
+
+	do {
+		*trav++ = *src;
+	} while (--dsize != 0 && *src++ != 0);
+
+	return dst;
+}
+
+uchar *ustrdup(const uchar *s) {
+	// Account for null-terminator
+	size_t count = ustrlen(s) + 1;
+
+	uchar *dup_s = malloc(count * sizeof(uchar));
+	if (dup_s == NULL) {
+		return NULL;
+	}
+
+	memcpy(dup_s, s, count);
+
+	return dup_s;
+}
+
+uchar *ustrndup(const uchar *s, size_t n) {
+	// Account for null-terminator
+	size_t count = ustrnlen(s, n) + 1;
+
+	uchar *dup_s = malloc(count * sizeof(uchar));
+	if (dup_s == NULL) {
+		return NULL;
+	}
+
+	// Copy one less and add null-terminator
+	memcpy(dup_s, s, count - 1);
+	dup_s[count - 1] = 0;
+
+	return dup_s;
+}
+
+size_t ustrlen(const uchar *ustr) {
+	size_t count = 0;
+	while (*ustr++ != 0) {
+		count++;
+	}
+
+	return count;
+}
+
+size_t ustrnlen(const uchar *ustr, size_t maxlen) {
+	size_t count = 0;
+	while (count < maxlen && *ustr++ != 0) {
+		count++;
+	}
+
+	return count;
+}
