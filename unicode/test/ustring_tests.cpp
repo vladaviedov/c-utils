3ef807764d40bc5e5d6ac60d8d6bd48838c2704b
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-04-30 14:32:39 -0400

    [unicode] strdup, strcpy tests

--- unicode/test/ustring_tests.cpp
+++ unicode/test/ustring_tests.cpp
@@ -12,8 +12,11 @@ namespace ustring_tests {
 	static const uchar empty_string[] = { 0 };
 	static const uchar test_string[]
 		= { U'こ', U'ん', U'に', U'ち', U'は', U'世', U'界', 0 };
+
 	static const uchar unterminated_string[]
 		= { U'h', U'e', U'l', U'l', U'o', 0x1234, 0x5678, 0 };
+	static const uchar terminated_string[]
+		= { U'h', U'e', U'l', U'l', U'o', 0 };
 
 	TEST(Unicode, Ustrlen) {
 		EXPECT_EQ(ustrlen(empty_string), 0);
@@ -25,6 +28,32 @@ namespace ustring_tests {
 		EXPECT_EQ(ustrnlen(test_string, 5), 5);
 		EXPECT_EQ(ustrnlen(test_string, 10), 7);
 		EXPECT_EQ(ustrnlen(unterminated_string, 5), 5);
+	}
+
+	TEST(Unicode, Ustrdup) {
+		uchar *copy_str = ustrdup(test_string);
+		EXPECT_EQ(memcmp(copy_str, test_string, sizeof(test_string)), 0);
+		free(copy_str);
+	}
+
+	TEST(Unicode, Ustrndup) {
+		uchar *copy_str = ustrndup(unterminated_string, 5);
+		EXPECT_EQ(memcmp(copy_str, terminated_string, sizeof(terminated_string)), 0);
+		free(copy_str);
+	}
+
+	TEST(Unicode, Ustrcpy) {
+		uchar copy_buf[8];
+		ustrcpy(copy_buf, test_string);
+
+		EXPECT_EQ(memcmp(copy_buf, test_string, sizeof(test_string)), 0);
+	}
+
+	TEST(Unicode, Ustrncpy) {
+		uchar copy_buf[5];
+		ustrncpy(copy_buf, unterminated_string, 5);
+
+		EXPECT_EQ(memcmp(copy_buf, unterminated_string, sizeof(uchar) * 5), 0);
 	}
 
 } // namespace ustring_tests
