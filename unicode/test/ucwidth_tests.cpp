8f128b8f8809a2f1da6ed2419b9e6e8b085c64aa
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-07-08 20:54:01 -0400

    [unicode] add ucswidth tests

--- unicode/test/ucwidth_tests.cpp
+++ unicode/test/ucwidth_tests.cpp
@@ -10,6 +10,13 @@ namespace ucwidth_tests {
 	static const uchar east_asian_chars[] = { U'好', U'い', U'한' };
 	static const uchar zero_width_chars[] = { 0x350, 0xdd3, 0x1d196 };
 
+	static const uchar empty_str[] = { 0x0 };
+	static const uchar with_control_str[]
+		= { U'h', U'e', U'l', U'l', U'o', 0x1, 0x0 };
+	static const uchar sample_str[]
+		= { U'天', U'気', U'が', U'い', U'い', U'か', U'ら', U'、', U'　',
+			  U'散', U'歩', U'し', U'ま', U'し', U'ょ', U'う', 0x0 };
+
 	TEST(Unicode, UcwidthControl) {
 		// Some C0 char
 		EXPECT_EQ(ucwidth(0x1b), -1);
@@ -44,6 +51,22 @@ namespace ucwidth_tests {
 		EXPECT_EQ(ucwidth(0xad), 1);
 		// Zero width space
 		EXPECT_EQ(ucwidth(0x200b), 0);
+	}
+
+	TEST(Unicode, UcswidthEmpty) {
+		EXPECT_EQ(ucswidth(empty_str, 256), 0);
+	}
+
+	TEST(Unicode, UcswidthControl) {
+		EXPECT_EQ(ucswidth(with_control_str, 256), -1);
+	}
+
+	TEST(Unicode, UcswidthString) {
+		EXPECT_EQ(ucswidth(sample_str, 256), 32);
+	}
+
+	TEST(Unicode, UcswidthLimited) {
+		EXPECT_EQ(ucswidth(sample_str, 16), 16);
 	}
 
 } // namespace ucwidth_tests
